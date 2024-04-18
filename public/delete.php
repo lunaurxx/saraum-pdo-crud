cd6e44b06fd32cf5830e2551d45b52383168106d
Saraum.M -IT24A <143664877+lunaurxx@users.noreply.github.com>
2024-04-18 11:41:00 +0800

    Update delete.php

--- public/delete.php
+++ public/delete.php
@@ -64,7 +64,7 @@ if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
                             <p>Are you sure you want to delete this employee record?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary ml-2">No</a>
+                                <a href="../index.php" class="btn btn-secondary ml-2">No</a>
                             </p>
                         </div>
                     </form>
