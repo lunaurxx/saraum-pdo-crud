21654d208383c2e2abb426c4fbfa0f67546b20d4
mariel <sarmariel@gmail.com>
2024-05-21 15:55:00 +0800

    fixed the functionality

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: ./products/index.html");
+    header("location: ./product/index.html");
     exit;
 }
  
